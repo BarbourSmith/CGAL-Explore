2fe8e32f4e159354e2a1a7e00e062f456387c34c
BarbourSmith <BarbourSmith@users.noreply.github.com>
2020-11-18 18:22:59 -0800

    Just before adding the 2D stuff

--- lib/canvas.cc
+++ lib/canvas.cc
@@ -9,6 +9,18 @@
 #include <CGAL/Nef_polyhedron_3.h>
 #include <iostream>
 
+typedef CGAL::Simple_cartesian<double>     Kernel;
+typedef CGAL::Polyhedron_3<Kernel>         Polyhedron;
+typedef Kernel::Point_3                    Point_3;
+typedef Polyhedron::Halfedge_handle        Halfedge_handle;
+typedef Polyhedron::Vertex_iterator        Vertex_iterator;
+typedef Polyhedron::Facet_iterator                   Facet_iterator;
+typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
+typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron;
+typedef Kernel::Vector_3  Vector_3;
+typedef Kernel::Aff_transformation_3  Aff_transformation_3;
+
+
 template <class Poly>
 typename Poly::Halfedge_handle make_cube_3( Poly& P) {
     // appends a cube of size [0,1]^3 to the polyhedron P.
@@ -36,110 +48,112 @@ typename Poly::Halfedge_handle make_cube_3( Poly& P) {
 }
 
 
-typedef CGAL::Simple_cartesian<double>     Kernel;
-typedef CGAL::Polyhedron_3<Kernel>         Polyhedron;
-typedef Kernel::Point_3                    Point_3;
-typedef Polyhedron::Halfedge_handle        Halfedge_handle;
-typedef Polyhedron::Vertex_iterator        Vertex_iterator;
-typedef Polyhedron::Facet_iterator                   Facet_iterator;
-typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
-typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron;
-typedef Kernel::Vector_3  Vector_3;
-typedef Kernel::Aff_transformation_3  Aff_transformation_3;
-
-
-
 float displayBuffer[1000];
 
 Nef_polyhedron translate(Nef_polyhedron shape, int x, int y, int z){
-    
+
     Aff_transformation_3 aff(CGAL::TRANSLATION, Vector_3(x, y, z));
     shape.transform(aff);
-    
+
     return shape;
 }
+
+Nef_polyhedron passingValue();
 
 extern "C"
 {
 
-int displayShape(){
-    
+  Nef_polyhedron * createNewNefCube(){
+    std::cout << "Making cube\n";
     Polyhedron P;
-    Halfedge_handle h = make_cube_3( P);
-    
-    Nef_polyhedron N1(P);
-    Nef_polyhedron initial(P);
-    
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            for(int k = 0; k < 3; k++){
-                printf("i:  %i\n", i);
-                Nef_polyhedron N2(initial);
-                N2 = translate(N2,2*i,2*j,2*k);
-                N1 += N2;
-            }
-        }
-    }
-    
+    make_cube_3( P);
+
+    Nef_polyhedron * newlyCreatedNefPolyhedron = new Nef_polyhedron(P);
+    return newlyCreatedNefPolyhedron;
+  }
+
+
+  int writeToDisplayBuffer(Nef_polyhedron* toDisplay){
+
+    Nef_polyhedron N1 = *toDisplay;
     Polyhedron P2;
     N1.convert_to_polyhedron(P2);
-    
-    
+
     printf("Facets before: %lu\n", P2.size_of_facets());
-    
+
     CGAL::Polygon_mesh_processing::triangulate_faces(P2);
-    
+
     printf("Facets after: %lu\n", P2.size_of_facets());
-    
+
     int index = 0;
-    
+
     for (  Facet_iterator i = P2.facets_begin(); i != P2.facets_end(); ++i) {
-        
+
         Halfedge_facet_circulator j = i->facet_begin();
-        
+
         Vertex_iterator v = j->vertex();
-        
+
         displayBuffer[index] = v->point()[0];
         displayBuffer[index+1] = v->point()[1];
         displayBuffer[index+2] = v->point()[2];
-        
+
         j++;
-        
+
         Vertex_iterator w = j->vertex();
-        
+
         displayBuffer[index+3] = w->point()[0];
         displayBuffer[index+4] = w->point()[1];
         displayBuffer[index+5] = w->point()[2];
-        
+
         j++;
-        
+
         Vertex_iterator x = j->vertex();
-        
+
         displayBuffer[index+6] = x->point()[0];
         displayBuffer[index+7] = x->point()[1];
         displayBuffer[index+8] = x->point()[2];
-        
+
         index = index + 9;
     }
-    
+
     return index;
-}
+  }
 
-float * getDisplayBufferAddress(){
+  int displayShape(){
+
+    Nef_polyhedron* myNefCube = createNewNefCube();
+    Nef_polyhedron* myNefCube2 = createNewNefCube();
+
+    Nef_polyhedron N1 = *myNefCube;
+    Nef_polyhedron initial = *myNefCube2;
+    Nef_polyhedron N2(initial);
+
+
+    N2 = translate(N2,4,4,0);
+    N1 += N2;
+
+    int index = writeToDisplayBuffer(&N1);
+
+    return index;
+  }
+
+  float * getDisplayBufferAddress(){
     return displayBuffer;
-}
+  }
 
+  Nef_polyhedron * createNewNefPolyhdron(){
+    Nef_polyhedron * newlyCreatedNefPolyhedron = new Nef_polyhedron;
+    return newlyCreatedNefPolyhedron;
+  }
 }
 
 
 // Init circle data and start render - JS
 int main(){
+    printf("Loading buffer...\n");
 
-    
-    printf("Loading...\n");
-    
     EM_ASM({ init(); });
-    
-    
+
+
     return 0;
 }
